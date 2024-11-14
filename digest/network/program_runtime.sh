a5f1c9165e1a05ef7aafa16ddfe0a2b6008fdc2c
Pierre <uitxj@student.kit.edu>
2024-11-14 15:42:39 +0100

    removed network folder from all folders

--- digest/network/program_runtime.sh
+++
@@ -1,2 +0,0 @@
-s1 $SDE/run_bfshell.sh -b /home/p4/Documents/me/digest/network/runtime_s1.py
-s2 $SDE/run_bfshell.sh -b /home/p4/Documents/me/digest/network/runtime_s2.py
--- meter/network/program_runtime.sh
+++
@@ -1,2 +0,0 @@
-s1 $SDE/run_bfshell.sh -b /home/p4/Documents/me/slice/network/runtime_s1.py
-s2 $SDE/run_bfshell.sh -b /home/p4/Documents/me/slice/network/runtime_s2.py
