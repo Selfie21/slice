c4052f5b9ad8021c69ebc96919b08d0daa081de4
Pierre <uitxj@student.kit.edu>
2024-12-06 15:07:43 +0100

    runtime for testing

--- network/program_runtime.sh
+++ network/program_runtime.sh
@@ -1,2 +1,2 @@
-s1 $SDE/run_bfshell.sh -b /home/p4/Documents/me/slice/network/runtime_s1.py
-s2 $SDE/run_bfshell.sh -b /home/p4/Documents/me/slice/network/runtime_s2.py
+s1 $SDE/run_bfshell.sh -b /home/p4/Documents/p4slice/slice/network/runtime_s1.py
+s2 $SDE/run_bfshell.sh -b /home/p4/Documents/p4slice/slice/network/runtime_s2.py
